7da9c8f7883f23fb8134862837be9507736ec2b7
wormyrocks <ekahn@hmc.edu>
2017-09-12 19:42:57 -0400

    Made progress on accel data. still incomplete

--- Assets/Scripts/Input.cs
+++ Assets/Scripts/Input.cs
@@ -11,15 +11,21 @@ public class Input : MonoBehaviour {
     void Start() {
         j = new Joycon();
         tr = GetComponent<Transform>();
-        //tr.Rotate(0.5f, 0.5f, 0.5f);
         if (j.attach() != -1) {
             j.init(0x0);
         }
+        j.poll();
+        j.set_zero_accel();
     }
 
     // Update is called once per frame
     void Update() {
        if (j.alive) { j.poll(); };
+        if ((j.buttons[0] & 0x80) != 0)
+        {
+            j.set_zero_accel();
+        }
+        tr.eulerAngles = new Vector3((float)(j.euler[0]), (float)(j.euler[1]), 0);
     }
     // FixedUpdate is called before physics are applied each frame
     private void FixedUpdate()
--- Assets/Scripts/Joycon.cs
+++ Assets/Scripts/Joycon.cs
@@ -6,29 +6,42 @@ using System.Runtime.InteropServices;
 using System;
 using UnityEngine;
 
-public class Joycon {
+public class Joycon
+{
 
     public bool isleft;
     public bool alive = false;
-    public int roll, pitch, yaw;
-    public int acx, acy, acz;
+
     public byte[] buttons = { 0x00, 0x00 };
     public Int16[] stick = { 0, 0 };
 
     private IntPtr handle;
 
     // Different operating systems either do or don't like the trailing zero
-	private const ushort vendor_id  = 0x57e;
+    private const ushort vendor_id = 0x57e;
     private const ushort vendor_id_ = 0x057e;
 
-	private const ushort product_l = 0x2006;
-	private const ushort product_r = 0x2007;
-	private byte[] default_buf = { 0x1, 0x0, 0x0, 0x1, 0x40, 0x40, 0x0, 0x1, 0x40, 0x40 };
+    private const ushort product_l = 0x2006;
+    private const ushort product_r = 0x2007;
+
+    private byte[] default_buf = { 0x1, 0x0, 0x0, 0x1, 0x40, 0x40, 0x0, 0x1, 0x40, 0x40 };
+
     private byte[] stick_raw = { 0, 0, 0 };
     private UInt16[] stick_cal = { 0, 0, 0, 0, 0, 0 };
     private UInt16[] stick_precal = { 0, 0 };
+
+    private UInt16[] acc = { 0, 0, 0 };
+    private double[] acc_f = { 0, 0, 0 };
+    private UInt16[] acc_g = { 0, 0, 0 };
+
+    private Int16[] gyr = { 0, 0, 0 };
+    public double[] euler = { 0, 0, 0 };
+    private const double alpha = 0.5;
+
     private byte global_count = 0;
     private const uint report_len = 48;
+
+
 
     public int attach()
     {
@@ -65,15 +78,15 @@ public class Joycon {
         handle = HIDapi.hid_open_path(enumerate.path);
         HIDapi.hid_set_nonblocking(handle, 1);
         HIDapi.hid_free_enumeration(ptr);
-        
+
         alive = true;
         return 0;
     }
     public void init(byte leds)
     {
-        byte[] a = {0x0};
+        byte[] a = { 0x0 };
         // Input report mode
-        subcommand(0x3, new byte[] {0x3f}, 1);
+        subcommand(0x3, new byte[] { 0x3f }, 1);
         a[0] = 0x1;
         subcommand(0x48, a, 1);
         subcommand(0x40, a, 1);
@@ -88,35 +101,54 @@ public class Joycon {
         a[0] = leds;
         printarray(subcommand(0x30, a, 1), report_len);
     }
+
     public void poll()
     {
         byte[] buf = new byte[report_len];
         HIDapi.hid_read_timeout(handle, buf, new UIntPtr(report_len), 50);
-        
+
         if (buf[0] != 0x30)
         {
             Debug.Log("Changing input mode to 0x30. If this happens more than once something is wrong.");
             subcommand(0x3, new byte[] { 0x30 }, 1);
+            return;
         }
-        else
-        {
-            roll = buf[13] | ((buf[14] << 8) & 0xff00);
-            pitch = buf[15] | ((buf[16] << 8) & 0xff00);
-            yaw = buf[17] | ((buf[18] << 8) & 0xff00);
-            acx = buf[19] | ((buf[20] << 8) & 0xff00);
-            acy = buf[21] | ((buf[22] << 8) & 0xff00);
-            acz = buf[23] | ((buf[24] << 8) & 0xff00);
-            buttons[0] = buf[2 + (isleft ? 2 : 0)];
-            buttons[1] = buf[3];
-            stick_raw[0] = buf[6 + (isleft ? 0 : 3)];
-            stick_raw[1] = buf[7 + (isleft ? 0 : 3)];
-            stick_raw[2] = buf[8 + (isleft ? 0 : 3)];
-            stick_precal[0] = (UInt16)(stick_raw[0] | ((stick_raw[1] & 0xf) << 8));
-            stick_precal[1] = (UInt16)((stick_raw[1] >> 4) | (stick_raw[2] << 4));
-            stick = center_sticks(stick_precal);
-            Debug.Log(stick[0] + " " + stick[1]);
-            //http://www.instructables.com/id/Accelerometer-Gyro-Tutorial/
-        }
+
+        gyr[0] = (Int16)(buf[13] | ((buf[14] << 8) & 0xff00));
+        gyr[1] = (Int16)(buf[15] | ((buf[16] << 8) & 0xff00));
+        gyr[2] = (Int16)(buf[17] | ((buf[18] << 8) & 0xff00));
+
+        acc[0] = (UInt16)((int)(buf[19] | ((buf[20] << 8) & 0xff00))+32767);
+        acc[1] = (UInt16)((int)(buf[21] | ((buf[22] << 8) & 0xff00))+32767);
+        acc[2] = (UInt16)((int)(buf[23] | ((buf[24] << 8) & 0xff00))+32767);
+        Debug.Log(acc[0]);
+
+        buttons[0] = buf[3 + (isleft ? 2 : 0)];
+        buttons[1] = buf[4];
+
+        stick_raw[0] = buf[6 + (isleft ? 0 : 3)];
+        stick_raw[1] = buf[7 + (isleft ? 0 : 3)];
+        stick_raw[2] = buf[8 + (isleft ? 0 : 3)];
+
+        stick_precal[0] = (UInt16)(stick_raw[0] | ((stick_raw[1] & 0xf) << 8));
+        stick_precal[1] = (UInt16)((stick_raw[1] >> 4) | (stick_raw[2] << 4));
+        stick = center_sticks(stick_precal);
+
+        for (int i = 0; i < 3; ++i)
+            acc_f[i] = (acc[i]) / 32768.0 * alpha + (acc_f[i] * (1.0f - alpha));
+
+        //https://theccontinuum.com/2012/09/24/arduino-imu-pitch-roll-from-accelerometer/
+        euler[0] = (Math.Atan2(-acc_f[1], acc_f[2]) * 180.0) / Math.PI;
+        euler[1] = (Math.Atan2(acc_f[0], Math.Sqrt(acc_f[1] * acc_f[1] + acc_f[2] * acc_f[2])) * 180.0) / Math.PI;
+
+        //http://www.instructables.com/id/Accelerometer-Gyro-Tutorial/
+    }
+    
+    public void set_zero_accel()
+    {
+        acc_g[0] = acc[0];
+        acc_g[1] = acc[1];
+        acc_g[2] = acc[2];
     }
 
     private Int16[] center_sticks(UInt16[] vals)
@@ -127,11 +159,11 @@ public class Joycon {
         {
             if (vals[i] > stick_cal[2 + i])
             {
-                s[i] = (Int16)((vals[i] - stick_cal[2 + i]) * -1.0f / stick_cal[i]*32768);
+                s[i] = (Int16)((vals[i] - stick_cal[2 + i]) * -1.0f / stick_cal[i] * 32768);
             }
             else
             {
-                s[i] = (Int16)((vals[i] - stick_cal[2 + i]) * 1.0f / stick_cal[4 + i]*32768);
+                s[i] = (Int16)((vals[i] - stick_cal[2 + i]) * 1.0f / stick_cal[4 + i] * 32768);
             }
         }
         return s;
@@ -191,9 +223,10 @@ public class Joycon {
     {
         byte[] buf = { addr2, addr1, 0x00, 0x00, (byte)len };
         byte[] ret = new byte[len];
-        byte[] buf_ = new byte[len+20];
+        byte[] buf_ = new byte[len + 20];
 
-        for (int i = 0; i < 100; ++i){
+        for (int i = 0; i < 100; ++i)
+        {
             buf_ = subcommand(0x10, buf, 5, false);
             if (buf_[15] == addr2 && buf_[16] == addr1)
             {
@@ -215,11 +248,12 @@ public class Joycon {
         }
         Debug.Log(tostr);
     }
-    private void printarray(UInt16[] arr, uint len) { 
+    private void printarray(UInt16[] arr, uint len)
+    {
         string tostr = "";
-        for (int i = 0; i<len; ++i)
+        for (int i = 0; i < len; ++i)
         {
-            tostr += string.Format("{0:D} ",arr[i]);
+            tostr += string.Format("{0:D} ", arr[i]);
         }
         Debug.Log(tostr);
     }

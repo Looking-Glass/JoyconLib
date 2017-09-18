91ecf128a262953320db46ff905dfc6c75dca917
wormyrocks <ekahn@hmc.edu>
2017-09-18 15:51:04 -0400

    refactoring

---
+++ Assets/JoyconDemo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyconDemo : MonoBehaviour {
+    private Joycon j;
+    private Transform tr;
+    // Use this for initialization
+    void Start ()
+    {
+        j = JoyconManager.Instance.j;
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        tr.eulerAngles = j.GetEulerAngles();
+        if (j.GetKeyPressed(Joycon.Button.SHOULDER_2))
+        {
+            Debug.Log("SDF");
+            j.Recenter();
+        }
+    }
+}
---
+++ Assets/Scripts/JoyconManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class JoyconManager: MonoBehaviour
+{
+    public Joycon j;
+
+    static JoyconManager instance;
+    public static JoyconManager Instance
+    {
+        get
+        {
+            if (instance != null) return instance;
+            instance = FindObjectOfType<JoyconManager>();
+            return instance;
+        }
+        private set { instance = value; }
+    }
+    void Awake()
+    {
+        instance = this;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        j = new Joycon();
+        j.Attach(alpha: .1f, leds: 0x0, imu: true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        j.Update();
+    }
+
+    void OnApplicationQuit()
+    {
+        j.Detach();
+    }
+}
